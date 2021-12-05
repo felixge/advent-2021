27b8fd43e45196fcf26df5d891ccbbb30e9e8535
Felix Geisendörfer <felix@felixge.de>
2021-12-05 05:41:19 -0800

    day-5-2 v1

---
+++ day-5-2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+	answer, err := Answer(strings.TrimSpace(string(input)) + "\n")
+	fmt.Printf("answer: %v\n", answer)
+	return err
+}
+
+// See v*.go files for Answer() implementations.
